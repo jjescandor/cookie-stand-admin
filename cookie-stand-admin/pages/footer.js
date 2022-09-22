0b0d6a8bae49e2ae0c341be7c6f61df0590ad191
JJ Escandor <jj.p.escandor@gmail.com>
2022-09-21 22:55:38 -0400

    Finished the lab

--- cookie-stand-admin/pages/footer.js
+++ cookie-stand-admin/pages/footer.js
@@ -4,7 +4,7 @@ import styles from '../styles/Home.module.css'
 export default function Footer() {
     return (
         <footer className="bg-emerald-600 h-10 my-auto flex flex-col content-center ">
-            <h2 className='my-auto'>&copy; 2021</h2>
+            <h2 className='my-auto text-2xl m-5'>&copy; 2021</h2>
         </footer>
     )
 }
--- cookie-stand-admin/pages/header.js
+++ cookie-stand-admin/pages/header.js
@@ -6,7 +6,7 @@ export default function Header() {
 
     return (
         <header className='bg-emerald-600 h-10  flex flex-col content-center'>
-            <h1 className="my-auto">Cookie Stand Admin</h1>
+            <h1 className="my-auto text-2xl m-5">Cookie Stand Admin</h1>
         </header>
     )
 }
