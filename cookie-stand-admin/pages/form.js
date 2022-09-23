187caecaa0386ba21eff6201caa61bdb76d4d787
JJ Escandor <jj.p.escandor@gmail.com>
2022-09-23 12:56:47 -0400

    updated styling

--- cookie-stand-admin/pages/form.js
+++ cookie-stand-admin/pages/form.js
@@ -6,20 +6,20 @@ export default function From(props) {
             <h1 className="m-4 text-2xl">Create A Cookie Stand</h1>
             <div className='flex flex-grow items-center justify-evenly w-full p-2'>
                 <label>Location</label>
-                <input name="loc" className='bg-sky-100 w-5/6 m-2 rounded' />
+                <input name="loc" className='bg-sky-100 w-5/6 m-2 rounded' required />
             </div>
             <div className="flex flex-row flex-grow items-center content-center justify-center w-full">
                 <div className='flex flex-grow flex-col items-center content-center justify-evenly bg-emerald-200 h-5/6 m-3 rounded'>
                     <label>Minimum Customer per Hour</label>
-                    <input type="number" name="min" className='bg-white w-5/6 rounded' />
+                    <input type="number" name="min" className='bg-white w-5/6 rounded' required />
                 </div>
                 <div className='flex flex-grow flex-col items-center content-center justify-evenly bg-emerald-200 h-5/6 m-3 rounded'>
                     <label>Maximum Customers per Hour</label>
-                    <input type="number" name="max" className='bg-white w-5/6 rounded' />
+                    <input type="number" name="max" className='bg-white w-5/6 rounded' required />
                 </div>
                 <div className='flex flex-grow flex-col items-center content-center justify-evenly bg-emerald-200 h-5/6 m-3'>
                     <label>Average Cookie per Sale</label>
-                    <input type="number" name="avg" className='bg-white w-5/6 rounded' />
+                    <input type="number" name="avg" className='bg-white w-5/6 rounded' required />
                 </div>
                 <button className='bg-emerald-600 flex-grow h-10 w-1/6 m-3 shadow-md rounded-md hover:bg-red-200'>Create</button>
             </div>
