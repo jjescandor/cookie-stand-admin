ac1f14e62364c27be9230ffc575c6f9b615aff3f
JJ Escandor <jj.p.escandor@gmail.com>
2022-09-25 21:09:55 -0400

    updated styling

--- cookie-stand-admin/pages/cookiestandadmin.js
+++ cookie-stand-admin/pages/cookiestandadmin.js
@@ -59,10 +59,10 @@ const CookieStandAdmin = () => {
                 <title>Cookie Stand Admin</title>
             </Head>
             <Header />
-            <main className='flex flex-col items-center h-5/6 w-full z-40'>
-                <CreateForm handleSubmit={handleSubmit} />
-                <OverviewModal modalIsOpen={modalIsOpen} setIsModalOpen={setIsModalOpen} input={input} className="w-full" />
-                <div className='flex flex-col items-center my-10'>
+            <main className='flex flex-col items-center h-5/6 overflow-scroll'>
+                <CreateForm handleSubmit={handleSubmit} className="z-0" />
+                <OverviewModal modalIsOpen={modalIsOpen} setIsModalOpen={setIsModalOpen} input={input} className="w-full z-40" />
+                <div className='flex flex-col items-center my-10 z-0'>
                     {input?.length <= 0 &&
                         <h1 className="text-2xl">
                             No Cookie Stands Available
@@ -73,8 +73,8 @@ const CookieStandAdmin = () => {
                     }
                 </div>
                 {input?.length > 0 &&
-                    <div className="flex">
-                        <button onClick={clearData} className='flex flex-row p-4 bg-emerald-700 rounded-md shadow-lg hover:bg-red-200 m-1'> <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="my-auto mx-3 w-6 h-6">
+                    <div className="flex z-30">
+                        <button onClick={clearData} className='flex flex-row p-4 bg-emerald-700 rounded-md shadow-lg hover:bg-red-200 m-1 z-30'> <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="my-auto mx-3 w-6 h-6">
                             <path fillRule="evenodd" d="M16.5 4.478v.227a48.816 48.816 0 013.878.512.75.75 0 11-.256 1.478l-.209-.035-1.005 13.07a3 3 0 01-2.991 2.77H8.084a3 3 0 01-2.991-2.77L4.087 6.66l-.209.035a.75.75 0 01-.256-1.478A48.567 48.567 0 017.5 4.705v-.227c0-1.564 1.213-2.9 2.816-2.951a52.662 52.662 0 013.369 0c1.603.051 2.815 1.387 2.815 2.951zm-6.136-1.452a51.196 51.196 0 013.273 0C14.39 3.05 15 3.684 15 4.478v.113a49.488 49.488 0 00-6 0v-.113c0-.794.609-1.428 1.364-1.452zm-.355 5.945a.75.75 0 10-1.5.058l.347 9a.75.75 0 101.499-.058l-.346-9zm5.48.058a.75.75 0 10-1.498-.058l-.347 9a.75.75 0 001.5.058l.345-9z" clipRule="evenodd" />
                         </svg>
                             CLEAR TABLE</button>
@@ -86,7 +86,7 @@ const CookieStandAdmin = () => {
                     </div>
                 }
             </main>
-            <Footer input={input} />
+            <Footer input={input} className="z-40" />
         </div >
     )
 }
