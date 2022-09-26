8042c0468d6dfa010160892d2539831c0d08bb0d
JJ Escandor <jj.p.escandor@gmail.com>
2022-09-25 21:18:08 -0400

    updated styling

--- cookie-stand-admin/components/createform.js
+++ cookie-stand-admin/components/createform.js
@@ -19,7 +19,7 @@ export const CreateForm = (props) => {
                     <label>Average Cookie per Sale</label>
                     <input type="number" name="avg" className='bg-white w-5/6 rounded' required />
                 </div>
-                <button className='flex flex-row bg-emerald-600 flex-grow h-10 m-3 shadow-md rounded-md hover:bg-red-200'> <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="my-auto mx-3 w-6 h-6">
+                <button className='flex flex-row bg-emerald-600 flex-grow items-center content-center h-4/5 m-3 m-auto shadow-md rounded-md hover:bg-red-200'> <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="my-auto mx-3 w-6 h-6">
                     <path fillRule="evenodd" d="M12 2.25c-5.385 0-9.75 4.365-9.75 9.75s4.365 9.75 9.75 9.75 9.75-4.365 9.75-9.75S17.385 2.25 12 2.25zM12.75 9a.75.75 0 00-1.5 0v2.25H9a.75.75 0 000 1.5h2.25V15a.75.75 0 001.5 0v-2.25H15a.75.75 0 000-1.5h-2.25V9z" clipRule="evenodd" />
                 </svg>
                     <h6 className='mx-auto my-auto'>Create</h6></button>
