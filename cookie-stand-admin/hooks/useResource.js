48013c7b8f6c1b299098bbbb05101823a1e2a3b1
JJ Escandor <jj.p.escandor@gmail.com>
2022-10-22 14:32:13 -0400

    fixed the api endpoint

--- cookie-stand-admin/hooks/useResource.js
+++ cookie-stand-admin/hooks/useResource.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import useSWR from 'swr';
 
-export const apiUrl = `/api/v1/cookie_stands/`;
+export const apiUrl = NEXT_PUBLIC_API_URL;
 import { useAuth } from '../contexts/auth';
 
 export default function useResource() {
